9a759c9275ed2b408f971631138782ef9ffba830
Erin Pronobis <erinpronobis@Erins-MacBook-Air.local>
2021-11-17 22:11:54 -0500

    first terraform example

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami             = "ami-04ad2567c9e3d7893"
+  instance_type   = "t3.medium"
+
+  tags = {
+    Name        = "ExampleAppWebInstance"
+    Environment = "test"
+    Owner       = "Erin Pronobis"
+    Application = "Apache Server"
+  }
+}
+
